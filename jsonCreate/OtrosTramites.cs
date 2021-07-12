348200079eedf3dbfec8c7f2d8fa481b10056a2f
leandromarelli <lemarell@everis.com>
2021-07-12 13:21:19 -0300

    jsonCreate

---
+++ jsonCreate/OtrosTramites.cs
@@ -0,0 +1,19 @@
+namespace jsonCreate
+{
+    internal class OtrosTramites
+    {
+        public int sfdc_opcion = 1;
+        public string sfdc_origen = "CEX";
+        public string sfdc_opcion_desc = "NoRecepcionFacturaOtroDomicilio";
+        public string sfdc_gen_rol = "EDESUREXPRESS";
+        public int sfdc_cod_medio = 17;
+        public string caso_tipo = "TIP003";
+        public string caso_motivo = "MOT003";
+        public string caso_submotivo = "ATAR189";
+        public string caso_estado = "ESTA003";
+        public string caso_origen = "CAN015";
+        public int caso_prioridad = 1;
+        public string caso_adjuntos = "N";
+        public string caso_buzon = "ARG_Canal_Digital";
+    }
+}
---
+++ jsonCreate/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace jsonCreate
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            OtrosTramites otram = new OtrosTramites();
+            
+            string JSONresult = JsonConvert.SerializeObject(otram);
+            
+            string path = @"C:\Users\lemarell\source\repos\jsonCreate\sfdc.cs"; 
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
